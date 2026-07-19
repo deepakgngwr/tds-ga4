6644f92b9e42828363c6aab0516cd4eb918a1808
deepakgngwr <22f1001001@ds.study.iitm.ac.in>
2026-07-20 05:04:19 +0530

    created config

---
+++ config.py
@@ -0,0 +1,6 @@
+EMAIL = "22f1001001@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjEwMDEwMDFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDUwMzkxMSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTEwODcxMX0.3nTyLR_ArkfiN7MwC-eJrP-MlyySp67C7u0fnbP4zSQ"
+
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o"
